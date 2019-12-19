a199b863a9216d3acde88a98661042af55498bcf
Darshit <darshitanjaria@gmail.com>
2019-12-18 20:46:30 -0800

    #13 Home Activity

--- app/src/main/java/com/example/mvvmsample/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/mvvmsample/ui/home/HomeActivity.kt
@@ -2,12 +2,27 @@ package com.example.mvvmsample.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.mvvmsample.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_fragment),
+            drawer_layout
+        )
     }
 }
