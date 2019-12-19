dd41654b0d7f7d9d3a9a1c918b37de30a657dd2a
Darshit <darshitanjaria@gmail.com>
2019-12-18 20:11:11 -0800

    #12 User Signup

--- app/src/main/java/com/example/mvvmsample/data/network/MyApi.kt
+++ app/src/main/java/com/example/mvvmsample/data/network/MyApi.kt
@@ -2,8 +2,6 @@ package com.example.mvvmsample.data.network
 
 import com.example.mvvmsample.data.network.responses.AuthResponse
 import okhttp3.OkHttpClient
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,7 +13,18 @@ interface MyApi {
 
     @FormUrlEncoded
     @POST("login")
-    suspend fun userLogin(@Field("email") email: String, @Field("password") password: String): Response<AuthResponse>
+    suspend fun userLogin(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<AuthResponse>
+
+    @FormUrlEncoded
+    @POST("signup")
+    suspend fun userSignup(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<AuthResponse>
 
     companion object {
 
--- app/src/main/java/com/example/mvvmsample/ui/auth/SignupActivity.kt
+++ app/src/main/java/com/example/mvvmsample/ui/auth/SignupActivity.kt
@@ -1,13 +1,58 @@
 package com.example.mvvmsample.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.mvvmsample.R
+import com.example.mvvmsample.data.db.entities.User
+import com.example.mvvmsample.databinding.ActivitySignupBinding
+import com.example.mvvmsample.ui.home.HomeActivity
+import com.example.mvvmsample.util.Hide
+import com.example.mvvmsample.util.Show
+import com.example.mvvmsample.util.snackbar
+import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class SignupActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+    private val factory: AuthViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
+        val binding: ActivitySignupBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_signup)
+        val viewModel = ViewModelProviders.of(this, factory).get(AuthViewModel::class.java)
+        binding.viewmodel = viewModel
+        viewModel.authListener = this
+
+        viewModel.getLoggedInUser().observe(this, Observer { user ->
+            if (user != null) {
+                Intent(this, HomeActivity::class.java).also {
+                    it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(it)
+                }
+            }
+        })
+
+    }
+
+    override fun onStarted() {
+        progress_login.Show()
+    }
+
+    override fun onSuccess(user: User) {
+        progress_login.Hide()
+    }
+
+    override fun onFailure(e: String) {
+        progress_login.Hide()
+        coordinator_login.snackbar(e)
     }
 }
