38cc7324ca52b62ab8669b3ae784306a6c255519
Darshit <darshitanjaria@gmail.com>
2019-12-19 23:48:58 -0800

    #16 Shared Preferences

--- app/src/main/java/com/example/mvvmsample/data/repositories/QuoteRepository.kt
+++ app/src/main/java/com/example/mvvmsample/data/repositories/QuoteRepository.kt
@@ -6,13 +6,19 @@ import com.example.mvvmsample.data.db.AppDatabase
 import com.example.mvvmsample.data.db.entities.Quote
 import com.example.mvvmsample.data.network.MyApi
 import com.example.mvvmsample.data.network.SafeApiRequest
+import com.example.mvvmsample.data.preferences.PreferenceProvider
 import com.example.mvvmsample.util.Coroutines
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+private const val MINIMUM_INTERVAL = 6
 
 class QuoteRepository(
     private val api: MyApi,
-    private val database: AppDatabase
+    private val database: AppDatabase,
+    private val preferences: PreferenceProvider
 ) : SafeApiRequest() {
 
     private val quotes = MutableLiveData<List<Quote>>()
@@ -31,18 +37,20 @@ class QuoteRepository(
     }
 
     private suspend fun fetchQuotes() {
-        if (isFetchNeeded()) {
+        val lastSavedAt = preferences.getLastSavedAt()
+        if (lastSavedAt == null || isFetchNeeded(LocalDateTime.parse(lastSavedAt))) {
             val response = apiRequest { api.getQuotes() }
             quotes.postValue(response.quotes)
         }
     }
 
-    private fun isFetchNeeded(): Boolean {
-        return true
+    private fun isFetchNeeded(savedAt: LocalDateTime): Boolean {
+        return ChronoUnit.HOURS.between(savedAt, LocalDateTime.now()) > MINIMUM_INTERVAL
     }
 
     private fun saveQuotes(quotes: List<Quote>) {
         Coroutines.io {
+            preferences.saveLastSavedAt(LocalDateTime.now().toString())
             database.getQuoteDao().saveAllQuotes(quotes)
         }
     }
